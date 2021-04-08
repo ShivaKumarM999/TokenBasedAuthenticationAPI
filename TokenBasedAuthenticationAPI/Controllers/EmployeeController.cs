8ce437a9cf4f78a9330f0e66ea21a29932dafb72
ShivaKumarM999 <manchikatlashivakumar999@gmail.com>
2021-04-08 15:50:01 +0530

    Add project files.

---
+++ TokenBasedAuthenticationAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TokenBasedAuthenticationAPI.Models;
+
+namespace TokenBasedAuthenticationAPI.Controllers
+{
+    [Authorize]
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+    }
+}
